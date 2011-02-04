6117f0b6d2d2d82b20365099426ca18d79ddd476
ksc1073 <ksc1073@localhost>
2011-02-03 19:52:39 -0500

    LineFollower updates

---
+++ 1073FRC11/LineFollower.cpp
@@ -0,0 +1,99 @@
+#include "LineFollower.h"
+
+LineFollower::LineFollower(LNDrive *d, Joystick *ljoy, Joystick *rjoy, DigitalInput *llsensor,  DigitalInput *mlsensor, DigitalInput *rlsensor, Encoder *lEnc, Encoder *rEnc)
+{
+	drive = d;
+	leftJoystick = ljoy;
+	rightJoystick = rjoy;
+	leftLineSensor = llsensor;
+	middleLineSensor = mlsensor;
+	rightLineSensor = rlsensor;
+	leftEncoder = lEnc;
+	rightEncoder = rEnc;
+	done = false; 
+}
+
+void LineFollower::PeriodicService()
+{
+//	if (inAutonomous || rightJoystick->GetRawButton(LineFollowButton))
+//	{
+		FollowLine();
+	//}
+	//else
+	//{
+	//	drive->StopOverride();
+	//}
+}
+
+void LineFollower::StartInAutonomous()
+{
+	inAutonomous = true;
+}
+
+void LineFollower::EndInAutonomous()
+{
+	inAutonomous = false;
+}
+
+void LineFollower::FollowLine(void)
+{	
+	const float maxSpeed = 0.4;
+	if (!done && leftEncoder->GetDistance()<14.0)
+	{
+		leftIsOn = leftLineSensor->Get();
+		middleIsOn = middleLineSensor->Get();
+		rightIsOn = rightLineSensor->Get();
+		
+		
+		if (middleIsOn && !leftIsOn && !rightIsOn)
+		{
+			drive->Override(maxSpeed, maxSpeed);
+		}
+		else if (leftIsOn && middleIsOn && !rightIsOn)
+		{
+			drive->Override(maxSpeed, maxSpeed / 2);
+		}
+		else if (rightIsOn && middleIsOn && !leftIsOn)
+		{
+			drive->Override(maxSpeed / 2, maxSpeed);
+		}
+		else if (leftIsOn && !middleIsOn && !rightIsOn)
+		{
+			drive->Override(maxSpeed, maxSpeed / 3);
+		}
+		else if (leftIsOn && rightIsOn && !middleIsOn)
+		{
+			drive->Override(maxSpeed, maxSpeed / 2);
+		}
+		else if (rightIsOn && !middleIsOn && !leftIsOn)
+		{
+			drive->Override(maxSpeed / 3, maxSpeed);
+		}
+		else if (leftIsOn && middleIsOn && rightIsOn)
+		{
+			done = true;
+			//drive->Override(-0.1, -0.1);
+			
+			//if (rightIsOn && leftIsOn && !middleIsOn)
+			//{
+				//fork = true;
+				//drive->Override(-0.25, -0.1);
+				//printf("fork\n");
+			//}
+			//else if (!rightIsOn && !leftIsOn && !middleIsOn)
+			//{
+					//drive->Override(-0.1, -0.1);
+					//printf("none\n");
+			//}
+				
+				// hang ubertube
+		} 
+		//printf("l:%d m:%d r:%d\n", leftIsOn, middleIsOn, rightIsOn);
+	}
+	else
+	{
+		drive->Override(0.0, 0.0);
+		//printf("l:%d m:%d r:%d\n", leftIsOn, middleIsOn, rightIsOn);
+	}
+}
+
